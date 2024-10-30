22239fec2d607a3296f6b0f7daf17c791829326c
Uche Ogwumike <ucheogwumike@gmail.com>
2024-10-30 11:56:39 +0100

    test

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -28,7 +28,7 @@ import { RolesService } from 'src/roles/roles.service';
         secure: true,
         auth: {
           user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASS,
+          pass: 'wazza1000', //process.env.MAIL_PASS,
         },
       },
       template: {
